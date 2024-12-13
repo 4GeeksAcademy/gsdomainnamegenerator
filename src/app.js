0136a4347c7e432cc5717ca402fddd3e1304f07d
1122gs <GORDON.SMITHJR@hotmail.com>
2024-12-13 23:41:42 +0000

    new commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io"];
+
+  for (let proInd = 0; proInd < pronoun.length; proInd++) {
+    for (let adjInd = 0; adjInd < adj.length; adjInd++) {
+      for (let nounInd = 0; nounInd < noun.lenght; nounInd++) {
+        for (let extInd = 0; extInd < extension.length; extInd++) {
+          console.log(
+            pronoun[proInd] + adj[adjInd] + noun[nounInd] + extension[extInd]
+          );
+        }
+      }
+    }
+  }
 };
